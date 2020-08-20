3dece97e6dbfbd23bb2520814a80765b252a6b99
bearinawolfpack <bearinawolfpack@gmail.com>
2020-08-20 09:31:42 -0400

    Done.

--- src/reducers/shoppingListItemReducer.js
+++ src/reducers/shoppingListItemReducer.js
@@ -1,15 +1,15 @@
 export default function shoppingListItemReducer(
 	state = {
-		items: []
+		items: [],
 	},
 	action
 ) {
 	switch (action.type) {
-		case 'INCREASE_COUNT':
+		case "INCREASE_COUNT":
 			return {
 				...state,
-				items: state.items.concat(state.items.length + 1)
-			}
+				items: state.items.concat(state.items.length + 1),
+			};
 		default:
 			return state;
 	}
